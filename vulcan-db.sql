8327d0322a474448087aa475a5fbd0e0c681e602
Dillan <dillanrosa@gmail.com>
2024-10-29 23:07:19 -0300

    Mudanças Cosméticas DB

--- vulcan-db.sql
+++ vulcan-db.sql
@@ -1,4 +1,4 @@
-create table FUNCIONARIO(
+CREATE TABLE FUNCIONARIO(
     id int AUTO_INCREMENT PRIMARY KEY,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     nome VARCHAR(100) NOT NULL,
@@ -6,29 +6,32 @@ create table FUNCIONARIO(
     meio_de_transporte VARCHAR(20),
     salario DECIMAL(10,2)
 );
-create table CLIENTE(
-id int AUTO_INCREMENT PRIMARY KEY,
-cpf VARCHAR(14) NOT NULL UNIQUE,
-nome VARCHAR(100) NOT NULL,
-tefefone VARCHAR(20),
-lugar_de_saida VARCHAR(100),
-lugar_de_destino VARCHAR(100),
-meio_de_transporte VARCHAR(100),
-horario_de_saida DATETIME
+
+CREATE TABLE CLIENTE(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    tefefone VARCHAR(20),
+    lugar_de_saida VARCHAR(100),
+    lugar_de_destino VARCHAR(100),
+    meio_de_transporte VARCHAR(100),
+    horario_de_saida DATETIME
 );
-create table VIAGEM(
-id int AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-duração TIME,
-horario_de_saida DATETIME,
-lugar_de_saida VARCHAR(100),
-lugar_de_destino VARCHAR(100),
-meio_de_transporte VARCHAR(100)
+
+CREATE TABLE VIAGEM(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    duração TIME,
+    horario_de_saida DATETIME,
+    lugar_de_saida VARCHAR(100),
+    lugar_de_destino VARCHAR(100),
+    meio_de_transporte VARCHAR(100)
 );
-create table VEICULO(
-id int AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100), 
-lugar_de_partida VARCHAR(100),
-lugar_de_destino VARCHAR(100),
-horario_de_saida TIME
+
+CREATE TABLE VEICULO(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100), 
+    lugar_de_partida VARCHAR(100),
+    lugar_de_destino VARCHAR(100),
+    horario_de_saida TIME
 );
